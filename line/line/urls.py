a5945c867570bdc07e483e5350f6b568308edd41
VeamVeat <vv1629728@gmail.com>
2021-10-23 19:33:05 +0300

    feat: create urls.py in apps users and added urlpatterns

--- line/line/urls.py
+++ line/line/urls.py
@@ -14,15 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
-from users.views import index
 
 from line import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='home'),
+    path('', include('users.urls')),
 ]
 
 if settings.DEBUG:
