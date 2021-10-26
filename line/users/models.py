9a3b415568c93c7994077c2c4f3f618675fa16f9
VeamVeat <vv1629728@gmail.com>
2021-10-26 14:19:45 +0300

    feat: create model Profile

--- line/users/models.py
+++ line/users/models.py
@@ -1,77 +1,34 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from line import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.core.validators import RegexValidator
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
-)
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import  AbstractUser
+
+from .managers import UserManager
 
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        """
-        Creates and saves a User with the given email, date of
-        birth and password.
-        """
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        user = self.model(
-            email=self.normalize_email(email),
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
-        user = self.create_user(
-            email,
-            password=password,
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
-
-
-class User(AbstractBaseUser):
+class User(AbstractUser):
+    username = None
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
         unique=True,
     )
 
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
-
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    def __str__(self):
-        return self.email
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
 
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return True
-
-
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    @property
-    def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
-        return self.is_admin
+def _user_directory_path(instance, filename):
+    return 'users/image_profile/user_{0}/{1}'.format(instance.user.id, filename)
 
 
 class Profile(models.Model):
@@ -86,12 +43,27 @@ class Profile(models.Model):
     region = models.CharField(max_length=50, verbose_name="Регион проживания")
     user = models.OneToOneField(User,
                                 on_delete=models.CASCADE,
-                                primary_key=True)
+                                null=False,
+                                unique=True)
+
+    image = models.ImageField(
+        default='users/image_profile/default.png',
+        upload_to=_user_directory_path,
+        verbose_name="Фото профиля"
+    )
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.email)
 
 
-
+@receiver(post_save, sender=User)
+def save_or_create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        try:
+            instance.profile.save()
+        except ObjectDoesNotExist:
+            Profile.objects.create(user=instance)
 
 
