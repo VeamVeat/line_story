13ead40764ac5fbef49cd6cb321c545555f17bb5
VeamVeat <vv1629728@gmail.com>
2021-10-30 18:02:02 +0300

    fix: fixed localization, model naming, profile and wallet creation

--- line/users/models.py
+++ line/users/models.py
@@ -1,4 +1,4 @@
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -6,14 +6,14 @@ from django.core.validators import RegexValidator
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 
-from .managers import UserManager
-from line.settings import AUTH_USER_MODEL
+from users.managers import UserManager
+from django.contrib.auth import get_user_model
 
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(
-        verbose_name='email address',
+        verbose_name=_('email address'),
         max_length=255,
         unique=True,
     )
@@ -27,21 +27,40 @@ class User(AbstractUser):
         verbose_name = _('user')
         verbose_name_plural = _('users')
 
+    @property
+    def full_name(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+    @full_name.setter
+    def full_name(self, value: str):
+        names = value.split(' ')
+        self.first_name = names[0]
+        self.last_name = names[1]
+
 
 def _user_directory_path(instance, filename):
     return 'users/image_profile/user_{0}/{1}'.format(instance.user.id, filename)
 
 
+def validate_age(value):
+    if value < 18:
+        raise ValidationError(
+            _('age must be at least 18.'),
+            params={'value': value},
+        )
+
+
 class Profile(models.Model):
 
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
-                                 message="Phone number must not consist "
-                                         "of space and requires country "
-                                         "code. eg : +79546748973")
+                                 message=_('phone number must not'
+                                           ' consist of space and '
+                                           'requires country code.'
+                                           ' eg : +79546748973'))
 
-    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True, verbose_name="Телефон")
-    age = models.PositiveIntegerField(default=0, verbose_name="Возраст")
-    region = models.CharField(max_length=50, verbose_name="Регион проживания")
+    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True, verbose_name=_('email address'))
+    age = models.PositiveIntegerField(validators=[validate_age], default=0, verbose_name=_('age'))
+    region = models.CharField(max_length=50, verbose_name=_('region of residence'))
     user = models.OneToOneField(User,
                                 on_delete=models.CASCADE,
                                 null=False,
@@ -50,41 +69,32 @@ class Profile(models.Model):
     image = models.ImageField(
         default='users/image_profile/default.png',
         upload_to=_user_directory_path,
-        verbose_name="Фото профиля"
+        verbose_name=_('profile photo')
     )
 
-    def __str__(self):
-        return 'Profile for user {}'.format(self.user.email)
 
-
-class WalletModel(models.Model):
-    user_id = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE,
-                                   related_name='wallet', verbose_name='Кошелёк пользователя')
-    ballance = models.PositiveIntegerField(verbose_name='баланс пользователя', default=0)
+class Wallet(models.Model):
+    user = models.OneToOneField(get_user_model, on_delete=models.CASCADE,
+                                related_name='wallet', verbose_name=_('user`s wallet'))
+    ballance = models.PositiveIntegerField(verbose_name=_('user balance'), default=0)
 
     class Meta:
-        unique_together = (("user_id", "id"),)
-        permissions = (("can_add_money", "Top up balance"),)
+        unique_together = (("user", "id"),)
+        permissions = (("can_add_money", "top up balance"),)
 
     def __str__(self):
-        return f'{self.user_id.last_name} - {self.ballance}'
-
-    @property
-    def full_name(self):
-        return f'{self.user_id.first_name} {self.user_id.last_name}'
+        return f'{self.ballance}'
 
 
 @receiver(post_save, sender=User)
-def save_or_create_profile(sender, instance, created, **kwargs):
+def create_profile_and_wallet(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-        WalletModel.objects.create(user_id=instance)
-    else:
-        try:
-            instance.profile.save()
-            instance.wallet.save()
-        except ObjectDoesNotExist:
-            Profile.objects.create(user=instance)
-            WalletModel.objects.create(user_id=instance)
+        Wallet.objects.create(user=instance)
 
+
+@receiver(post_save, sender=User)
+def save_profile_and_wallet(sender, instance, **kwargs):
+    instance.profile.save()
+    instance.wallet.save()
 
