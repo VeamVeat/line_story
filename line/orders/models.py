6567c34d10dbee26dc14c7656c5d63b6f8e7ab26
VeamVeat <vv1629728@gmail.com>
2021-11-04 00:20:40 +0300

    fix: changing the field type

--- line/orders/models.py
+++ line/orders/models.py
@@ -17,7 +17,7 @@ class Order(CreatedAtMixin):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     slug = models.SlugField(null=False, unique=True)
     quantity = models.PositiveIntegerField(default=1, verbose_name=_('number of products in the order'))
-    final_price = models.DecimalField(max_digits=12, decimal_places=2, default=0,
+    final_price = models.DecimalField(max_digits=12, decimal_places=2,
                                       validators=[MinValueValidator(Decimal('0.01'))],
                                       verbose_name=_('total order price'))
     product_list = JSONField()
