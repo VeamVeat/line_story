6f919046eeccc07bf68accaf5ea616bb13faf808
VeamVeat <vv1629728@gmail.com>
2021-11-10 15:02:15 +0300

    fix: sending email after registration

--- line/line/urls.py
+++ line/line/urls.py
@@ -18,13 +18,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
 
-from users.views import register
+from users.views import ActivateAccount, SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('register/', register, name="register"),
+    path('register/', SignUpView.as_view(), name='register'),
+    path('activate/<uidb64>/<token>/', ActivateAccount.as_view(), name='activate'),
 ]
 
 if settings.DEBUG:
--- line/users/views.py
+++ line/users/views.py
@@ -1,23 +1,72 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.urls import reverse
+from django.views import View
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib import messages
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
+from django.utils.translation import ugettext_lazy as _
+
 
 from users.forms import RegisterUserForm
+from users.tokens import account_activation_token
+from users.models import User
 
 
 def home(request):
     return render(request, "users/dashboard.html")
 
 
-def register(request):
-    if request.method == "GET":
-        return render(
-            request, "users/register.html",
-            {"form": RegisterUserForm}
-        )
-    elif request.method == "POST":
-        form = RegisterUserForm(request.POST)
-        if form.is_valid():
-            user = form.save()
+class ActivateAccount(View):
+    def get(self, request, uidb64, token, *args, **kwargs):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError):
+            user = None
+
+        if user is not None and account_activation_token.check_token(user, token):
+            user.is_active = True
+            user.save()
             login(request, user)
-            return redirect(reverse("home"))
+            return redirect('home')
+        else:
+            messages.warning(request, _('The confirmation link was invalid,'
+                                        ' possibly because it has already been used.'))
+            return redirect('home')
+
+
+class SignUpView(View):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+
+            current_site = get_current_site(request)
+            subject = 'Activate Your MySite Account'
+            message = render_to_string('registration/account_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+
+            messages.success(request, _('Please Confirm your email to complete registration.'))
+
+            return redirect('login')
+
+        return render(request, self.template_name, {'form': form})
